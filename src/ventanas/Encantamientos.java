cf76c3c416b5a87ab2f5b5fc01bdbe1c59835588
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2024-03-13 21:41:35 -0600

    ComboBox add in Enchants Window

--- src/ventanas/Encantamientos.java
+++ src/ventanas/Encantamientos.java
@@ -2,54 +2,117 @@ package ventanas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.Objects;
+import static java.awt.Font.BOLD;
 
-public class Encantamientos extends javax.swing.JFrame {
+public class Encantamientos extends JFrame {
     public Encantamientos() {
         initComponents();
-        agregarFondo();
     }
 
     private void initComponents() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(600, 650));
 
-        // Componentes de la ventana
-        JLabel jLabel1 = new JLabel("Encantamientos");
-        JLabel jLabel2 = new JLabel("IMAGEN");
-        JLabel jLabel3 = new JLabel("ESCRITO");
+        JLabel WindowEnchantTittle = new JLabel("Encantamientos", SwingConstants.CENTER);
+        WindowEnchantTittle.setFont(new Font("Segoe UI", BOLD, 30));
         JButton jButtonVolver = new JButton("Volver");
-
+        jButtonVolver.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/imagenes/arrowBack.png"))));
         jButtonVolver.addActionListener(this::jButtonVolverActionPerformed);
+
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.addItem("Seleccione una opción");
+        comboBox.addItem("Casco");
+        comboBox.addItem("Peto");
+        comboBox.addItem("Grebas");
+        comboBox.addItem("Botas");
+
+        JTextArea textArea = new JTextArea(10, 20);
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+        textArea.setText("Seleccione una pieza de armadura");
+        JScrollPane scrollPane = new JScrollPane(textArea);
 
         setLayout(null);
 
-        // Agregar componentes a la ventana con coordenadas absolutas
-        jLabel1.setBounds(250, 20, 150, 30);
-        jLabel2.setBounds(50, 100, 100, 100);
-        jLabel3.setBounds(300, 100, 250, 100);
+        WindowEnchantTittle.setBounds(175, 75, 250, 30);
+        comboBox.setBounds(200, 150, 200, 30);
+        scrollPane.setBounds(100, 250, 400, 300);
         jButtonVolver.setBounds(10, 10, 100, 30);
 
-        add(jLabel1);
-        add(jLabel2);
-        add(jLabel3);
+        add(WindowEnchantTittle);
+        add(comboBox);
+        add(scrollPane);
         add(jButtonVolver);
 
-        JLabel jLabel1Fondo = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("/imagenes/backgroundEnchants.jpg"))));
-        jLabel1Fondo.setBounds(0, 0, 600, 650);
-        add(jLabel1Fondo);
+        JLabel backgroundEnchants = new JLabel(new ImageIcon(Objects.requireNonNull(getClass().getResource("/imagenes/backgroundEnchants.jpg"))));
+        backgroundEnchants.setBounds(0, 0, 600, 650);
+        add(backgroundEnchants);
+
+        comboBox.addActionListener(e -> {
+            String option = (String) comboBox.getSelectedItem();
+            switch (option) {
+                case "Casco":
+                    textArea.setText("""
+                            Afinidad acuática (1)
+                            Respiración (1-3)
+                            Espinas(1-3)
+                            Protección(1-4)
+                            Protección contra explosiones(1-4)
+                            Protección contra fuego(1-4)
+                            Irrompibilidad(1-3)
+                            Reparación(1)
+                            """);
+                    break;
+                case "Peto":
+                    textArea.setText("""
+                            Espinas(1-3)
+                            Protección(1-4)
+                            Protección contra explosiones(1-4)
+                            Protección contra fuego(1-4)
+                            Irrompibilidad(1-3)
+                            Reparación(1)
+                            """);
+                    break;
+                case "Grebas":
+                    textArea.setText("""
+                            Furtivo rápido(1-3)
+                            Espinas(1-3)
+                            Protección(1-4)
+                            Protección contra explosiones(1-4)
+                            Protección contra fuego(1-4)
+                            Irrompibilidad(1-3)
+                            Reparación(1)
+                            """);
+                    break;
+                case "Botas":
+                    textArea.setText("""
+                            Agilidad acuática(1-3)
+                            Caida de pluma(1-4)
+                            Espinas(1-3)
+                            Protección(1-4)
+                            Protección contra explosiones(1-4)
+                            Protección contra fuego(1-4)
+                            Irrompibilidad(1-3)
+                            Reparación(1)
+                            """);
+                    break;
+                case null:
+                    break;
+                default:
+                    textArea.setText("Seleccione una pieza de armadura");
+            }
+        });
 
         pack();
     }
 
-    private void jButtonVolverActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jButtonVolverActionPerformed(ActionEvent evt) {
         // Cerrar la ventana actual (Encantamientos) y mostrar la ventana principal (Interfaz)
         this.dispose();
         Interfaz ventanaPrincipal = new Interfaz();
         ventanaPrincipal.setVisible(true);
-    }
-
-    private void agregarFondo() {
     }
 
     public static void main(String[] args) {
